f6a878994d8149c7be451357952191de98b0f09a
Nicolas Massé <nicolas.masse@itix.fr>
2017-05-18 18:07:58 +0100

    sample pvc

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,14 @@
+oc project default
 oc create -f service-account.yaml
 oc create -f roles.yaml
-# oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:test-provisioner:hostpath-provisioner
-# oc adm policy add-cluster-role-to-user hostpath-provisioner-runner system:serviceaccount:test-provisioner:hostpath-provisioner
+oc adm policy add-scc-to-user hostmount-anyuid -z hostpath-provisioner
+oc adm policy add-cluster-role-to-user hostpath-provisioner -z hostpath-provisioner
+
+mkdir /tmp/openshift
+chmod 777 /tmp/openshift
+chcon -Rt svirt_sandbox_file_t /tmp/openshift
 
 oc create -f pod.yaml
 oc create -f class.yaml
+
+oc create -f sample-claim.yaml
